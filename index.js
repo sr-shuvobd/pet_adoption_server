cc7dea919e76208bf481981d292d110ade9088b6
sr-shuvobd <mdshohanurrahomanshuvo@gmail.com>
2026-05-21 17:24:01 +0600

    add mongo bd

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
 const express = require('express');
+const dotenv = require('dotenv');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+dotenv.config();
 
 const app = express();
-const port = 5000;
-app.get('/', (req ,res)=> {
-    res.send('Server is Running')
-})
+const port = process.env.PORT;
+
+app.use(express.json());
+
+const uri = process.env.MONGO_URI;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
 app.listen(port, () =>{
     console.log(`The server is run ${port} port`)
 })
